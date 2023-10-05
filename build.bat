d4db369938c8a5218080a3b76f7a96f010fac286
Kirdow <10155603+Kirdow@users.noreply.github.com>
2023-10-05 18:02:59 +0200

    rename gfx.asm to wnd.asm

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ mkdir build
 mkdir dist
 @REM ASM FILES
 nasm -f win64 -o build\main.obj src\main.asm
-nasm -f win64 -o build\gfx.obj src\gfx.asm
+nasm -f win64 -o build\wnd.obj src\wnd.asm
 @REM LINK FILES
-link build\gfx.obj build\main.obj libs\SDL2.lib kernel32.lib msvcrt.lib legacy_stdio_definitions.lib /subsystem:console /entry:entry /out:dist\isogame.exe
+link build\wnd.obj build\main.obj libs\SDL2.lib kernel32.lib msvcrt.lib legacy_stdio_definitions.lib /subsystem:console /entry:entry /out:dist\isogame.exe
 @REM COPY FILES
 copy libs\SDL2.dll dist\SDL2.dll
